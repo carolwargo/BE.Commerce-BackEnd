982cf860472166391953717d8f8e4cc2178b9c1e
Carol Wargo <Carolwargo@gmail.com>
2023-06-07 19:13:45 -0400

    update

--- models/index.js
+++ models/index.js
@@ -1,60 +1,61 @@
-const Product = require('./Product');
-const Category = require('./Category'); 
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize(
+  "your_database",
+  "your_username",
+  "your_password",
+  {
+    host: "localhost",
+    dialect: "mysql",
+  }
+);
 
-/*
-// Set up the associations
-Product.belongsTo(Category, { foreignKey: 'category_id' });
-ProductTag.belongsTo(Product, { foreignKey: 'product_id' });
-ProductTag.belongsTo(Tag, { foreignKey: 'tag_id' });
+const Category = Category(sequelize, Sequelize);
+const Product = Product(sequelize, Sequelize);
+const Tag = Tag(sequelize, Sequelize);
+const ProductTag = ProductTag(sequelize, Sequelize);
 
-module.exports = {
-  Category,
-  Product,
-  Tag,
-  ProductTag,
-};*/
-
-
-
-Product.belongsTo(Category, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE'
+// DEFINE associations
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
 });
 
-ProductTag.belongsTo(Product, {
-  foreignKey: 'product_id'});
-
-ProductTag.belongsTo(Tag, {
-  foreignKey: 'tag_id'});
-    
-
-Category.hasMany(Product, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE'
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
 });
 
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as:'product_tags',
-  foreignKey: 'product_id',
-  onDelete: 'CASCADE'
+  as: "product_tags",
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
 });
 
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as:'product_tags',
-  foreignKey: 'tag_id',
-  onDelete: 'CASCADE'
+  as: "product_tags",
+  foreignKey: "tag_id",
+  onDelete: "CASCADE",
 });
 
+ProductTag.belongsTo(Product, {
+  foreignKey: "product_id",
+});
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: "tag_id",
+});
+
+// EXPORT models
 module.exports = {
   Category,
   Product,
   Tag,
   ProductTag,
 };
-
-
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const { Category, Product, Tag, ProductTag } = require('./models/index'); //updated path
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
