687a7450d0224e3877989e67cdd4483b8fb33e5d
Carol Wargo <Carolwargo@gmail.com>
2023-06-07 21:54:00 -0400

    update

--- models/Product.js
+++ models/Product.js
@@ -1,8 +1,7 @@
 //CHECK TAG ID ASSOCIATION
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
-const Product = require("Product");
-
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const Category = require("../models/Category");
 
 // INITIALIZE Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
