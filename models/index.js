74dde8395b5ed1fe2c53cdd82698a5f5a1e21f50
Carol Wargo <Carolwargo@gmail.com>
2023-06-07 21:01:37 -0400

    removed code

--- models/index.js
+++ models/index.js
@@ -3,22 +3,6 @@ const Category = require("./Category");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
-const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize(
-  "your_database",
-  "your_username",
-  "your_password",
-  {
-    host: "localhost",
-    dialect: "mysql",
-  }
-);
-
-const Category = Category(sequelize, Sequelize);
-const Product = Product(sequelize, Sequelize);
-const Tag = Tag(sequelize, Sequelize);
-const ProductTag = ProductTag(sequelize, Sequelize);
-
 // DEFINE associations
 Category.hasMany(Product, {
   foreignKey: "category_id",
