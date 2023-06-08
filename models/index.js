b30aa2ba9ef5dc6fba3a74f34bcd608d93e04697
Carol Wargo <Carolwargo@gmail.com>
2023-06-08 09:39:27 -0400

    update associations

--- models/index.js
+++ models/index.js
@@ -4,25 +4,41 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // DEFINE associations
-Product.belongsTo(Category, {
-  foreignKey: 'category_id',
+
+//PRODUCT belongs to one Category model
+Product.hasOne(Category, {
+  foreignKey: 'product_id',
   onDelete: 'CASCADE'
 });
 
+//CATEGORY has many Product models
 Category.hasMany(Product, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE'
 });
 
+//PRODUCT belongs to many Tag models
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: "product_id",
 });
 
+//TAG belongs to many Product models
 Tag.belongsToMany(Product, {
-  through: ProductTag,
   foreignKey: "tag_id",
 });
+
+//ALL PRODUCT have multiple TAG
+Product.hasMany(Tag, {
+  foreignKey: "product_id",
+});
+
+Tag. hasMany(Product, {
+  through: ProductTag,
+  foreignKey: "productTag_id",
+})
+
+
 
 // EXPORT models
 module.exports = { Category, Product, Tag, ProductTag };
