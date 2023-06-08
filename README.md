2a6a3053ed615bd0583ea24d96d4f83264bd794f
Carol Wargo <Carolwargo@gmail.com>
2023-06-08 17:08:10 -0400

    add associations

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # BE.Commerce-BackEnd
 
+Installation
+npm install 
+express 
+sequelize 
+dotenv
+path
+fs
+config
+
+
+
+
+Usage
 
 
 In Insomnia, the user tests 
@@ -12,20 +25,3 @@ In Insomnia, the user tests
 “GET tags,” 
 “GET Categories,” and 
 “GET All Products.”
-----------------------------------------
-GET PRODUCTS:
-“GET One Product.”
-“GET All Products.”
-
-GET CATEGORY:
-“DELETE Category by ID,” 
-“CREATE Category,” and 
-“UPDATE Category.”
-“GET Category by ID,”
-“GET Categories,”
-
-GET TAG:
-“GET tag by id,” 
-“GET tags,” 
-
-
--- models/index.js
+++ models/index.js
@@ -1,50 +1,37 @@
-"use strict";
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
 
-const fs = require("fs");
-const path = require("path");
-const Sequelize = require("sequelize");
-const process = require("process");
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
-const db = {};
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
-
-fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
-      file.indexOf(".") !== 0 &&
-      file !== basename &&
-      file.slice(-3) === ".js" &&
-      file.indexOf(".test.js") === -1
-    );
-  })
-  .forEach((file) => {
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+// DEFINE associations
+// PRODUCT belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey:'category_id',
+  onDelete: 'CASCADE',
 });
+// CATEGORY have many Product
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
+// PRODUCT belongToMany Tag (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id',
+  },
+});
+// TAGS belongToMany Product (through ProductTag)
+Tag.belongsToMany(Product, {
+  through:{
+    model: ProductTag,
+    foreignKey: 'tag_id',
+  },
+});
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
